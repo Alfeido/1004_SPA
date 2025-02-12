9b52e9884a4aa2106c3cacff154f45c5288c8d56
Alfeido <jenkinsalfie08@gmail.com>
2025-02-12 14:44:08 +0000

    Added images for card game. created a way to also submit json files and it be read

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+document.getElementById('uploadedFile').addEventListener('change', onChange);
+
+function onChange(event) {
+    var reader = new FileReader();
+    reader.onload = readerLoad;
+    reader.readAsText(event.target.files[0]);
+}
+
+function readerLoad(event){
+    console.log(event.target.result);
+    var obj = JSON.parse(event.target.result);
+    loadText(obj)
+}
+
+function loadText(file){
+    let heading = document.querySelector("h2");
+    heading.innerHTML = JSON.stringify(file, null, 2);
+}
